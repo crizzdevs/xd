c6faae1a767e4cc4d95375e03d264fe271a05d54
crizzdevs <abuancrisjulian1206@gmail.com>
2026-01-12 09:15:29 +0800

    Add Abuan_OOP class with HelloOOP functionalityxd

---
+++ Abuan_OOP.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to
+ * change this license
+ */
+package com.mycompany.introtooop;
+
+/**
+ *
+ * @author M304User
+ */
+public class Abuan_OOP {
+    public static void main(String[] args) {
+        HelloOOP xd = new HelloOOP();
+        xd.hellooop();
+    }
+}
+
+class HelloOOP {
+    void hellooop() {
+        System.out.println(
+            "Welcome to Object-Oriented Programming! OOP makes code reusable, modular, and easier to maintain."
+        );
+    }
+}
